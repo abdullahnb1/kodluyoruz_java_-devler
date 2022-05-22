2e46af4b1c83bed1d8b5e835c3f437dab0b3a9d0
Abdullah Naci Bodur <abdullahnacibodur@gmail.com>
2022-05-22 21:13:40 +0300

    First commit

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,36 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        //Değişkenleri tanımladık.
+        int matematik, fizik, kimya, türkce, tarih, müzik;
+
+        //İnput almak için scanner sınıfını tanımladık.
+        Scanner input = new Scanner(System.in);
+
+        //Kullanıcıya hangi değeri girmesi gerektiğini söylüyoruz.
+        System.out.print("Matematik sonuç: ");
+        matematik = input.nextInt();
+
+        System.out.print("Fizik sonuç: ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya sonuç: ");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe sonuç: ");
+        türkce = input.nextInt();
+
+        System.out.print("Tarih sonuç: ");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik sonuç: ");
+        müzik = input.nextInt();
+
+        String sonuc = ((matematik + fizik + kimya + türkce + tarih + müzik) / 6 >= 60) ? "Geçti":"Sınıfta Kaldı";
+
+        System.out.print(sonuc);
+    }
+}
