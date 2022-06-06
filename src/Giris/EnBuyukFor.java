2900b93a3ae4d3bc1a0a6167191f87305432bc93
Abdullah Naci Bodur <abdullahnacibodur@gmail.com>
2022-06-06 21:27:56 +0300

    En Büyük ve En Küçük Yapıldı.

---
+++ src/Giris/EnBuyukFor.java
@@ -0,0 +1,40 @@
+package Giris;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EnBuyukFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int sayi;
+        int tekrar;
+        int enBuyuk = 0;
+        int enKucuk = 0;
+
+
+        System.out.print("Lutfen kac sayi gireceginizi tuslayın: ");
+        sayi = input.nextInt();
+
+        for (int i = 0; i < sayi; i ++){
+            System.out.print("Lutfen bir sayi girin: ");
+            tekrar = input.nextInt();
+
+            if (i == 1){
+                enBuyuk = tekrar;
+                enKucuk = tekrar;
+            }
+            if (tekrar < enKucuk){
+                enKucuk = tekrar;
+            }
+            if (tekrar > enBuyuk){
+                enBuyuk = tekrar;
+            }
+
+        }
+        System.out.print("En buyuk => " + enBuyuk + "\nEn kucuk => " + enKucuk);
+
+    }
+}
